9634a8595bdd05b0a1e176d5590796dadd642e11
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-11 19:36:00 +0800

    feat: add sideSched style, add initial sideSched function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Consts
 const timerCont = document.querySelector("#TIMER-CONTAINER");
+const schedCont = document.querySelector("#SCHEDULE-CONTAINER")
+const landSect = document.querySelector("#LANDING-SECTION")
 const timerInput = document.querySelectorAll(".timerinput");
 let prev_s_input;
 let prev_m_input;
@@ -8,14 +10,22 @@ let day
 const resetButton = document.querySelector("#RESET-BTN");
 const startstopButton = document.querySelector("#STARTSTOP-BTN");
 const pauseButton = document.querySelector("#PAUSE-BTN");
-
 const sschedButton = document.querySelector("#SHOW-SCHEDULE-BTN");
+
+const schedBlock = document.querySelectorAll(".scheduleblock");
+
+const mon = schedBlock[0].cloneNode(true)
+const tue = schedBlock[1].cloneNode(true)
+const wed = schedBlock[2].cloneNode(true)
+const thu = schedBlock[3].cloneNode(true)
+const fri = schedBlock[4].cloneNode(true)
 
 // Date Obtainer
 document.addEventListener("DOMContentLoaded", () => {
-    today = new Date();
-    day = today.toDateString().split(" ")
-    console.log(day[0]);
+    // today = new Date();
+    // day = today.toDateString().split(" ")
+    // console.log(day[0]);
+    day = "Wed"
 });
 
 // Timer Input Functionalities
@@ -57,11 +67,31 @@ timerInput.forEach((inputs) => {
 });
     
 // Button Functionalities
-resetButton.addEventListener("click", (event) => {
+resetButton.addEventListener("click", () => {
     const MINS = document.querySelector("#MINUTES");
     const SECS = document.querySelector("#SECONDS");
 
     MINS.value = 10;
-    SECS.value = "00"
+    SECS.value = "00";
 });
 
+sschedButton.addEventListener("click", () => {
+    schedCont.classList.toggle("hidden")
+
+    if (day == "Mon") {
+        mon.classList.add("side-sched")
+        schedCont.appendChild(mon)
+    } else if (day == "Tue") {
+        tue.classList.add("side-sched")
+        schedCont.appendChild(tue)
+    } else if (day == "Wed") {
+        wed.classList.add("side-sched")
+        schedCont.appendChild(wed)
+    } else if (day == "Thu") {
+        thu.classList.add("side-sched")
+        schedCont.appendChild(thu)
+    } else if (day == "Fri") {
+        fri.classList.add("side-sched")
+        schedCont.appendChild(fri)
+    };
+});
