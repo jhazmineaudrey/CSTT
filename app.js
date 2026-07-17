3abe1fb689e3f8045495f76affdac84afa03ce7d
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-18 00:06:15 +0800

    feat: add initial responsive design to schedule shower

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const landSect = document.querySelector("#LANDING-SECTION")
 const timerInput = document.querySelectorAll(".timerinput");
 let prev_s_input;
 let prev_m_input;
-let day
-let interval
+let day;
+let interval;
 
 const MINS = document.querySelector("#MINUTES");
 const SECS = document.querySelector("#SECONDS");
-let timeLeft
+let timeLeft;
 
 const resetButton = document.querySelector("#RESET-BTN");
 const startstopButton = document.querySelector("#STARTSTOP-BTN");
@@ -19,12 +19,18 @@ const sschedButton = document.querySelector("#SHOW-SCHEDULE-BTN");
 
 const schedBlock = document.querySelectorAll(".scheduleblock");
 
-const mon = schedBlock[0].cloneNode(true)
-const tue = schedBlock[1].cloneNode(true)
-const wed = schedBlock[2].cloneNode(true)
-const thu = schedBlock[3].cloneNode(true)
-const fri = schedBlock[4].cloneNode(true)
-const weekend = schedBlock[5].cloneNode(true)
+const mon = schedBlock[0].cloneNode(true);
+const tue = schedBlock[1].cloneNode(true);
+const wed = schedBlock[2].cloneNode(true);
+const thu = schedBlock[3].cloneNode(true);
+const fri = schedBlock[4].cloneNode(true);
+const weekend = schedBlock[5].cloneNode(true);
+
+const MediaQuery = window.matchMedia("(max-width: 768px)");
+
+mediaChange();
+MediaQuery.addEventListener("change", mediaChange);
+sschedButton.addEventListener("click", showSched);
 
 // Date Obtainer
 document.addEventListener("DOMContentLoaded", () => {
@@ -96,82 +102,103 @@ resetButton.addEventListener("click", () => {
     clearInterval(interval);
     startstopButton.innerHTML = "Start";
     pauseButton.innerHTML = "Pause";
+    MINS.disabled = false;
+    SECS.disabled = false;
 });
 
-sschedButton.addEventListener("click", async () => {
-    sschedButton.children[0].classList.toggle("spin");
+// MediaQuery-Based Show Schedule Button Functionality
 
-    const first = timerCont.getBoundingClientRect()
-
-    schedCont.classList.toggle("hidden");
-
-    if (schedCont.classList.contains("hidden")) {
-        schedCont.style.pointerEvents = "none"
-
-        const parentRect = landSect.getBoundingClientRect();
-        const childRect = schedCont.getBoundingClientRect();
-
-        const reltop = childRect.top - parentRect.top
-        const relleft = childRect.left - parentRect.left
-
-        schedCont.style.width = `${childRect.width}px`;
-        schedCont.style.height = `${childRect.height}px`;
-
-        schedCont.style.top = `${reltop}px`;
-        schedCont.style.left = `${relleft}px`;
-
-        schedCont.style.position = "absolute"
-
-        schedCont.addEventListener("transitionend", (transition) => {
-            if (transition.propertyName === "opacity") {
-                schedCont.style.top = "";
-                schedCont.style.left = "";
-            }
-        })
+async function showSched() {
+    if (MediaQuery.matches) {
+        schedCont.classList.toggle("hidden");
+        schedCont.style.zIndex = "999";
+        schedCont.children[0].style.width = "80dvw"
     } else {
-        schedCont.style.pointerEvents = "all"
-        schedCont.style.position = "static"
-    };
+        sschedButton.children[0].classList.toggle("spin");
 
-    if (day == "Mon") {
-        mon.classList.add("side-sched");
-        schedCont.appendChild(mon);
-    } else if (day == "Tue") {
-        tue.classList.add("side-sched");
-        schedCont.appendChild(tue);
-    } else if (day == "Wed") {
-        wed.classList.add("side-sched");
-        schedCont.appendChild(wed);
-    } else if (day == "Thu") {
-        thu.classList.add("side-sched");
-        schedCont.appendChild(thu)
-    } else if (day == "Fri") {
-        fri.classList.add("side-sched");
-        schedCont.appendChild(fri);
-    } else {
-        weekend.classList.add("side-sched");
-        weekend.style.display = "flex";
-        schedCont.appendChild(weekend);
+        const first = timerCont.getBoundingClientRect();
+
+        schedCont.classList.toggle("hidden");
+
+        if (schedCont.classList.contains("hidden")) {
+            schedCont.style.pointerEvents = "none";
+
+            const parentRect = landSect.getBoundingClientRect();
+            const childRect = schedCont.getBoundingClientRect();
+
+            const reltop = childRect.top - parentRect.top;
+            const relleft = childRect.left - parentRect.left;
+
+            schedCont.style.width = `${childRect.width}px`;
+            schedCont.style.height = `${childRect.height}px`;
+
+            schedCont.style.top = `${reltop}px`;
+            schedCont.style.left = `${relleft}px`;
+
+            schedCont.style.position = "absolute";
+
+            schedCont.addEventListener("transitionend", (transition) => {
+                if (transition.propertyName === "opacity") {
+                    schedCont.style.top = "";
+                    schedCont.style.left = "";
+                };
+            });
+        } else {
+            schedCont.style.pointerEvents = "all"
+            schedCont.style.position = "static"
+        };
+
+        if (day == "Mon") {
+            mon.classList.add("side-sched");
+            schedCont.appendChild(mon);
+        } else if (day == "Tue") {
+            tue.classList.add("side-sched");
+            schedCont.appendChild(tue);
+        } else if (day == "Wed") {
+            wed.classList.add("side-sched");
+            schedCont.appendChild(wed);
+        } else if (day == "Thu") {
+            thu.classList.add("side-sched");
+            schedCont.appendChild(thu)
+        } else if (day == "Fri") {
+            fri.classList.add("side-sched");
+            schedCont.appendChild(fri);
+        } else {
+            weekend.classList.add("side-sched");
+            weekend.style.display = "flex";
+            schedCont.appendChild(weekend);
+        }
+
+        const last = timerCont.getBoundingClientRect();
+
+        const deltaX = first.left - last.left ;
+        const deltaY = first.top - last.top;
+
+        const timerContPosAnim = timerCont.animate([{
+            transformOrigin: "top left",
+            transform: `
+                translate(${deltaX}px, ${deltaY}px)
+            `
+        }, {
+            transformOrigin: "top left",
+            transform: "none"
+        }], {
+            duration: 1000,
+            easing: "ease"
+        });
     }
+};
 
-    const last = timerCont.getBoundingClientRect();
+function mediaChange() {
+    if (MediaQuery.matches) {
+        sschedButton.children[0].innerHTML = "👁";
+    } else {
+        sschedButton.children[0].innerHTML = "<";
+    }
+};
+    
 
-    const deltaX = first.left - last.left ;
-    const deltaY = first.top - last.top;
 
-    const timerContPosAnim = timerCont.animate([{
-        transformOrigin: "top left",
-        transform: `
-            translate(${deltaX}px, ${deltaY}px)
-        `
-    }, {
-        transformOrigin: "top left",
-        transform: "none"
-    }], {
-        duration: 1000,
-        easing: "ease"
-    });
-});
 
 // TIMER FUNCTIONALITY
 const updateTimer = () => {
